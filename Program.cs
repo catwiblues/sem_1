fcc0ab81844079fb67f67487c3eac535b6d7cff3
Danil Shcheblykin <catwiblues42@gmail.com>
2023-12-30 17:00:11 +0700

    added first task

--- Program.cs
+++ Program.cs
@@ -8,7 +8,22 @@
 
 // тип_данных имя_переменной = значение;
 // camelCase: firstNumber, secondNumber
-int number = 9; // number = 9
-int result = number * number; //81
+// int number = 9; // number = 9
+// int result = number * number; //81
 
+// Console.ReadLine() - получить информацию из строчки
+Console.Write("Введите первое число: "); // a
+int firstNumber = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите второе число: "); // b
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+// firstNumber = secondNumber * secondNumber
+if (firstNumber == secondNumber * secondNumber) // true
+// a = 5; a == 5? (true, false)
+{
+    Console.WriteLine("Да, " + firstNumber + " является квадратом от числа " + secondNumber);
+}
+else // a не является кв от чилса b
+{
+    Console.WriteLine("Нет, " + firstNumber + " не является квадратом числа " + secondNumber);
+}
